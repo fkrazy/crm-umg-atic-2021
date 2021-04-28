fae6f7479ab129fe025722870a07ae705b4b1631
frank orozco <frank@iqsoftware.com.gt>
2021-04-27 22:05:06 -0600

    deploy on github pages

--- vue.config.js
+++ vue.config.js
@@ -33,5 +33,9 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/crm-umg-atic-2021/'
+    : '/'
 };
+

78decedf9a2e9c3b6f9db21eb37ab38b3f22ad88
frank orozco <frank@iqsoftware.com.gt>
2021-05-01 01:03:39 -0600

    fix report routing

--- src/router/routes.js
+++ src/router/routes.js
@@ -43,7 +43,7 @@ const routes = [
             component: ClientEdit
           },
           {
-            path: 'report',
+            path: 'report/:id',
             name: 'reporte',
             component: ClientReport
           }

e39cbf8d1b6eaf7426ed3885e5f4fd231ee4311f
frank orozco <frank@iqsoftware.com.gt>
2021-05-15 08:26:08 -0600

    deploy

---
+++ precache-manifest.5d09d005833d173a814e8b099f084682.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/crm-umg-atic-2021/apple-icon.png"
+  },
+  {
+    "revision": "bbc4f79f3130f9a4f773",
+    "url": "/crm-umg-atic-2021/css/app.0641731e.css"
+  },
+  {
+    "revision": "5bc9e0dfcea3c21260c2",
+    "url": "/crm-umg-atic-2021/css/chunk-vendors.b4f840da.css"
+  },
+  {
+    "revision": "7f143c4bf897ba308fbb",
+    "url": "/crm-umg-atic-2021/css/common.15095842.css"
+  },
+  {
+    "revision": "e9f06cb06fd600500582",
+    "url": "/crm-umg-atic-2021/css/dashboard.0e433876.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/crm-umg-atic-2021/favicon.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/crm-umg-atic-2021/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/crm-umg-atic-2021/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/crm-umg-atic-2021/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/crm-umg-atic-2021/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "/crm-umg-atic-2021/img/anime3.png"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "/crm-umg-atic-2021/img/anime6.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "/crm-umg-atic-2021/img/apple-icon.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "/crm-umg-atic-2021/img/bg5.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/crm-umg-atic-2021/img/default-avatar.png"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "/crm-umg-atic-2021/img/emilyz.jpg"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "/crm-umg-atic-2021/img/favicon.png"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "/crm-umg-atic-2021/img/img_3115.jpg"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "/crm-umg-atic-2021/img/james.jpg"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "/crm-umg-atic-2021/img/mike.jpg"
+  },
+  {
+    "revision": "85f90b9e1f65142a93ee461089cb5c94",
+    "url": "/crm-umg-atic-2021/index.html"
+  },
+  {
+    "revision": "bbc4f79f3130f9a4f773",
+    "url": "/crm-umg-atic-2021/js/app.17743591.js"
+  },
+  {
+    "revision": "5bc9e0dfcea3c21260c2",
+    "url": "/crm-umg-atic-2021/js/chunk-vendors.ea82a91b.js"
+  },
+  {
+    "revision": "7f143c4bf897ba308fbb",
+    "url": "/crm-umg-atic-2021/js/common.8eea6a84.js"
+  },
+  {
+    "revision": "86e087a7ec195f63d283",
+    "url": "/crm-umg-atic-2021/js/common~dashboard.76d0b4bc.js"
+  },
+  {
+    "revision": "e9f06cb06fd600500582",
+    "url": "/crm-umg-atic-2021/js/dashboard.da12f38b.js"
+  },
+  {
+    "revision": "1a167cfd82a1a7480ea601fccc8d7e18",
+    "url": "/crm-umg-atic-2021/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm-umg-atic-2021/robots.txt"
+  }
+]);
